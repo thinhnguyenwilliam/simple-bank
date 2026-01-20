820c49b2ab8689b63d534b43e521e68a87db0645
ThinhNguyen <thinhproee@gmail.com>
2026-01-20 22:28:28 +0700

    fix deadlock

--- db/sqlc/store_test.go
+++ db/sqlc/store_test.go
@@ -1,3 +1,4 @@
+// simple-bank\db\sqlc\store_test.go
 package db
 
 import (
@@ -109,4 +110,56 @@ func TestTransferTx(t *testing.T) {
 	fmt.Println(">>[2] After: ", updatedAccount1.Balance, updatedAccount2.Balance)
 	require.Equal(t, account1.Balance-int64(n)*amount, updatedAccount1.Balance)
 	require.Equal(t, account2.Balance+int64(n)*amount, updatedAccount2.Balance)
+}
+
+func TestTransferTxDeadlock(t *testing.T) {
+	store := NewStore(testDB)
+
+	account1 := createRandomAccount(t)
+	account2 := createRandomAccount(t)
+	fmt.Println(">>[1] Before:", account1.Balance, account2.Balance)
+
+	n := 10
+	amount := int64(10)
+
+	errs := make(chan error)
+
+	for i := 0; i < n; i++ {
+		fromAccountID := account1.ID
+		toAccountID := account2.ID
+
+		if i%2 == 1 {
+			fromAccountID = account2.ID
+			toAccountID = account1.ID
+		}
+		txName := fmt.Sprintf("tx %d", i+1)
+
+		// ✅ pass variables into goroutine
+		go func(fromID, toID int64) {
+			_, err := store.TransferTx(context.WithValue(context.Background(), txKey, txName), TransferTxParams{
+				FromAccountID: fromID,
+				ToAccountID:   toID,
+				Amount:        amount,
+			})
+			errs <- err
+		}(fromAccountID, toAccountID)
+	}
+
+	// check results
+	for i := 0; i < n; i++ {
+		err := <-errs
+		require.NoError(t, err)
+	}
+
+	// check final balances (should be unchanged)
+	updatedAccount1, err := testQueries.GetAccount(context.Background(), account1.ID)
+	require.NoError(t, err)
+
+	updatedAccount2, err := testQueries.GetAccount(context.Background(), account2.ID)
+	require.NoError(t, err)
+
+	fmt.Println(">>[2] After:", updatedAccount1.Balance, updatedAccount2.Balance)
+
+	require.Equal(t, account1.Balance, updatedAccount1.Balance)
+	require.Equal(t, account2.Balance, updatedAccount2.Balance)
 }
