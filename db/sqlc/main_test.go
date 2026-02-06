6dea117b610bf522b6d700f152c7093d395fa891
ThinhNguyen <thinhproee@gmail.com>
2026-02-06 14:22:47 +0700

    fix(ci):postgres host v7

--- db/sqlc/main_test.go
+++ db/sqlc/main_test.go
@@ -6,29 +6,28 @@ import (
 	"os"
 	"testing"
 
-	"simple-bank/util"
-
 	_ "github.com/lib/pq"
 )
 
 var testQueries *Queries
 var testDB *sql.DB
 
-const dbDriver = "postgres"
-
 func TestMain(m *testing.M) {
-	// from db/sqlc → project root
-	config, err := util.LoadConfig("../..")
-	if err != nil {
-		log.Fatal("cannot load config:", err)
+	dbSource := os.Getenv("DB_SOURCE")
+	if dbSource == "" {
+		log.Fatal("DB_SOURCE is not set")
 	}
 
-	testDB, err = sql.Open(dbDriver, config.DBSource)
+	var err error
+	testDB, err = sql.Open("postgres", dbSource)
 	if err != nil {
 		log.Fatal("cannot connect to db:", err)
 	}
 
 	testQueries = New(testDB)
 
-	os.Exit(m.Run())
+	code := m.Run()
+
+	testDB.Close()
+	os.Exit(code)
 }
